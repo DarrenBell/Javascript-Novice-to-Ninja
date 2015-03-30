c1ec0c11b56bad744d16c4cb90eedec7441b980c
Darren Bell <darren.l.bell@gmail.com>
2015-03-31 09:37:33 +1100

    Notes for Programming basics in JavaScript
    Created a series of notes within a JS file as comments, this is to help
    me understand JavaScript more.

---
+++ JavaScriptNotes.js
@@ -0,0 +1,73 @@
+// Programming Basics in JavaScript - Notes
+
+// two forward slashes represent a comment
+
+/*
+
+This is another type of comment which goes
+over multiple lines
+
+It is considered good practice to put comments into your code
+*/
+
+/* JavaScript is written in C-Style syntax
+A JavaScript program is made up of a series of statements and each statement ends with a new line or semicolon (;)
+
+example of two statements (one on each line):
+
+a = "Hello World!";
+alert(a);
+
+A block is a series of statements that are collected together inside curly brackets:
+
+{
+	// this is a block containing two statements
+	var a = "Hello World";
+	alert(a);
+}
+
+Blocks do not need to be terminated by a semicolon
+Whitespace can be used as much as you like - indenting code for example and nesting code
+
+*/
+
+/* JavaScript has six different types of values - There are five primitive data types
+
+1. String
+2. Number
+3. Boolean (True / False)
+4. undefined
+5. null
+
+Any value that isn't one of the primitive data types listed is an object(6.)
+
+*/
+
+/* JavaScript has an Operator called "typeof" which finds out the type of a value - examples:
+
+typeof "hello"
+"string"
+
+typeof 10
+"number"
+
+typeof true
+"boolean"
+
+typeof { ninja: "turtle"}
+"object"
+
+*/
+
+/* Operators - An Operator applies an operation to a value, which is known as the operand.  There are three different operands:
+
+1. unary operator - only requires one operand
+typeof "hello" - the operator is typeof and the string "hello" is the operand
+2. binary operator - requires two operands
+3 + 5 - the operator is the + sign  The numbers 3 and 5 are the operands
+3. ternary operator - requires three operands
+
+*/
+
+
+
